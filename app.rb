bbdeaad1a1a84a60abd80f9cf18d9ae544c2b4b5
Tyler Geneva <ctg55@yahoo.com>
2015-06-09 16:04:41 -0500

    some psql for heroku

--- app.rb
+++ app.rb
@@ -2,9 +2,14 @@ require 'bundler'
 Bundler.require()
 
 ###################
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/march_madness')
 ActiveRecord::Base.establish_connection(
-  :adapter => 'postgresql',
-  :database => 'march_madness'
+ :adapter => 'postgresql',
+ :host     => db.host,
+ :username => db.user,
+ :password => db.password,
+ :database => db.path[1..-1],
+ :encoding => 'utf8'
 )
 ###################
 require './models/team'
